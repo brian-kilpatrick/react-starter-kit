69ac15476ab26bd5a978e70d0ec4b98d48fc20ca
Brian O. Kilpatrick <bkilpatrick@judge.com>
2017-05-08 23:39:24 -0400

    fixing git issue

--- src/data/models/User.js
+++ src/data/models/User.js
@@ -1,38 +1,34 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
+// 'use strict';
+// module.exports = function(sequelize, DataTypes) {
+//   var User = sequelize.define('User', {
+//     email: DataTypes.STRING,
+//     name: DataTypes.STRING
+//   });
+//   return User;
+// };
 
 import DataType from 'sequelize';
 import Model from '../sequelize';
 
 const User = Model.define('User', {
-
-  id: {
-    type: DataType.UUID,
-    defaultValue: DataType.UUIDV1,
-    primaryKey: true,
-  },
-
   email: {
     type: DataType.STRING(255),
     validate: { isEmail: true },
   },
-
-  emailConfirmed: {
-    type: DataType.BOOLEAN,
-    defaultValue: false,
+  firstName: {
+    type: DataType.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: DataType.STRING,
+    allowNull: false,
   },
 
 }, {
 
-  indexes: [
-    { fields: ['email'] },
-  ],
+  // indexes: [
+  //   { fields: ['email'] },
+  // ],
 
 });
 
